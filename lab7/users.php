6c3572c78a7d16fc0bafb2a97f528f421e980dee
Filippov Sergei <filippov.sergei.alex@gmail.com>
2026-03-04 12:28:11 +0300

    Add files via upload

---
+++ lab7/users.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+// 1. Исправляем namespace здесь (было App, стало src)
+use src\Classes\User;
+use src\Classes\SuperUser;
+
+spl_autoload_register(function ($class): void {
+    // 2. Упрощаем автозагрузчик. 
+    // Поскольку namespace (src\Classes) теперь совпадает с папкой (src/Classes), 
+    // нам просто нужно заменить обратные слэши на прямые.
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+echo "<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <meta charset='UTF-8'>
+    <title>User Info</title>
+    <link rel=\"stylesheet\" href=\"css/style.css\">
+</head>
+<body>";
+
+// Эти вызовы теперь сработают, так как они ссылаются на src\Classes
+$user1 = new User("Роман", "rom", "pass1");
+$user2 = new User("Анна", "anna", "pass2");
+
+echo $user1->showInfo();
+echo $user2->showInfo();
+
+$user = new SuperUser("Admin", "w_admin", "pass4", "administrator");
+echo $user->showInfo();
+
+unset($user1, $user2, $user);
+
+echo "</body></html>";
